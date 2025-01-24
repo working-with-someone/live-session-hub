31366cc35778d311146058795ab80e1be8271d35
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-01-24 18:26:58 +0900

    add ffmpeg process pool

---
+++ src/lib/ffmpeg/ffmpegProcessPool.ts
@@ -0,0 +1,89 @@
+import { ChildProcessWithoutNullStreams, spawn } from 'node:child_process';
+import { Socket } from 'socket.io';
+import path from 'node:path';
+import httpStatusCode from 'http-status-codes';
+import { wwsError } from '../../error/wwsError';
+
+class FFmpegProcessPool {
+  pool: Map<string, ChildProcessWithoutNullStreams>;
+
+  constructor() {
+    this.pool = new Map();
+  }
+
+  private createProcess(liveSessionId: string) {
+    const rtmpEndpoint = path.join(
+      process.env.RTMP_SERVER_ORIGIN as string,
+      'live',
+      liveSessionId
+    );
+
+    return spawn('ffmpeg', [
+      '-y',
+      // input, output file의 format을 강제한다. 지정하지 않으면 auto detection한다.
+      '-f',
+      'webm',
+      // 0 : stdin, 1 : stdout, 2 : stderr 를 의미한다.
+      '-i',
+      'pipe:0',
+      // video codec을 libx264로 지정한다.
+      '-c:v',
+      'libx264',
+      // audio code을 aac로 지정한다.
+      '-c:a',
+      'aac',
+      // 압축 속도를 조절한다. 압축속도가 높을수록 압축률은 줄어든다.
+      '-preset',
+      'medium',
+      '-f',
+      'flv',
+      rtmpEndpoint,
+    ]);
+  }
+
+  getOrCreateProcess(liveSessionId: string) {
+    let process = this.pool.get(liveSessionId);
+
+    // get
+    if (process) {
+      return process;
+    }
+
+    // create
+    process = this.createProcess(liveSessionId);
+
+    process.on('error', (err) => {
+      // handle
+    });
+
+    // process가 종료되면 pool에서 제거한다.
+    process.on('exit', () => {
+      this.pool.delete(liveSessionId);
+    });
+
+    this.pool.set(liveSessionId, process);
+
+    return process;
+  }
+
+  terminateProcess(liveSessionId: string) {
+    const process = this.pool.get(liveSessionId);
+
+    if (!process) {
+      return;
+    }
+
+    process.stdin.end();
+    process.kill();
+  }
+
+  terminateAllProcess() {
+    for (const liveSessionId of this.pool.keys()) {
+      this.terminateProcess(liveSessionId);
+    }
+  }
+}
+
+const ffmpegProcessPool = new FFmpegProcessPool();
+
+export default ffmpegProcessPool;
