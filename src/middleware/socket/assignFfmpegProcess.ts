739c56ef1e5b93ae84669f765c03e42a5e0fe117
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-02-14 20:15:17 +0900

    add : resume sub processes's stdio pipe for prevent pipe blocking

--- src/middleware/socket/assignFfmpegProcess.ts
+++ src/middleware/socket/assignFfmpegProcess.ts
@@ -11,6 +11,24 @@ const assignFfmpegProcessToOrganizer = async (
   if (socket.liveSession.role === Role.organizer) {
     const process = ffmpegProcessPool.getOrCreateProcess(socket.liveSession.id);
 
+    process.stdin.on('error', (err) => {
+      console.log(err);
+    });
+
+    // ffmpeg는 진행 상태, message들을 stderr로 출력한다.
+    // spawn과정에서 stdio를 ignore하지 않았기 때문에 정보가 필요하지 않더라도, 이 pipe를 resume해주지 않으면 pipe blocking이 발생한다.
+    process.stderr.on('data', (data) => {
+      console.log('stderr : ', data.toString());
+    });
+
+    process.stdout.on('data', (data) => {
+      console.log('stdout ; ', data.toString());
+    });
+
+    process.on('error', (err) => {
+      console.log(err);
+    });
+
     socket.ffmpegProcess = process;
 
     socket.on('disconnect', () => {
