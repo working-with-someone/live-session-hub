34e6dbbc00b0fb2efd45367aabd2a818a4fa32d5
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-09-01 01:20:48 +0900

    fix : participant of live session A can recive transition event from live session B

--- src/handler/transitionHandler.ts
+++ src/handler/transitionHandler.ts
@@ -1,24 +1,24 @@
-import { Namespace, Socket } from "socket.io";
-import WS_CHANNELS from "../constants/channels";
-import { ResponseCb } from "../@types/augmentation/socket/response";
-import httpStatusCode from "http-status-codes";
+import { Namespace, Socket } from 'socket.io';
+import WS_CHANNELS from '../constants/channels';
+import { ResponseCb } from '../@types/augmentation/socket/response';
+import httpStatusCode from 'http-status-codes';
 
 const registerTransitionHandler = (nsp: Namespace, socket: Socket) => {
   // naming은 break가 reserved 라서
   const opened = (cb: ResponseCb) => {
-    nsp.emit(WS_CHANNELS.transition.broadCast.open)
+    socket.nsp.emit(WS_CHANNELS.transition.broadCast.open);
 
-    cb({ status: httpStatusCode.OK })
-  }
+    cb({ status: httpStatusCode.OK });
+  };
 
   const breaked = (cb: ResponseCb) => {
-    nsp.emit(WS_CHANNELS.transition.broadCast.break)
+    socket.nsp.emit(WS_CHANNELS.transition.broadCast.break);
 
-    cb({ status: httpStatusCode.OK })
-  }
+    cb({ status: httpStatusCode.OK });
+  };
 
   socket.on(WS_CHANNELS.transition.open, opened);
-  socket.on(WS_CHANNELS.transition.break, breaked)
-}
+  socket.on(WS_CHANNELS.transition.break, breaked);
+};
 
-export default registerTransitionHandler
+export default registerTransitionHandler;
